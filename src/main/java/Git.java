eaa685296b2c24fb767d98aae21116d0d2ac7034
root <1506045619@qq.com>
2022-02-21 18:14:12 +0800

    push-commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -6,5 +6,6 @@ public class Git {
         System.out.println("hot-fix");
         System.out.println("master-冲突test");
         System.out.println("冲突test");
+        System.out.println("push");
     }
 }
